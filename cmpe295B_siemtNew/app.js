756a29ab20a1e905ef0bf6512c69b66d9c94c7bb
FNU SHILPA <fshilpa@us.ibm.com>
2015-11-25 17:50:39 -0800

    added pie and funnel

--- cmpe295B_siemtNew/app.js
+++ cmpe295B_siemtNew/app.js
@@ -19,10 +19,10 @@ var io=require('socket.io');
 var smtpTransport = nodemailer.createTransport("SMTP",{
 	service: "Gmail",
 	auth: {
-	user: "chilukuri3@gmail.com",
-	pass: "deepthi999"
+		user: "chilukuri3@gmail.com",
+		pass: "deepthi999"
 	}
-	});
+});
 
 //all environments
 app.set('views', __dirname + '/views');
@@ -42,7 +42,7 @@ app.use(multer());
 
 
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 
@@ -57,102 +57,125 @@ app.get("/ruleEngine",function(req,res){
 
 
 app.get("/chart",function(req,res){
-	 var dates=[];
-	 var temp=[];
-	 var finalJson=[];
-	 mongo.getSplineChartData(function(err,reslt){
-	  if(err){
-	   throw err;
-	  }else{
-	   var recs=JSON.parse(JSON.stringify(reslt));
-	   console.log(reslt.length);
-	   
-	   var date;
-	   for(var k=0;k<reslt.length;k++){
-	    if(reslt[k]._id!=""){
-	    date = Date.parse(reslt[k]._id);
-	    var objTemp=[];
-	    var ambTemp=[];
-	        objTemp[0] = date;
-	        ambTemp[0]= date;
-	        objTemp[1] = reslt[k].objTemp;
-	        ambTemp[1] = reslt[k].ambTemp;
-	       dates.push(objTemp);
-	       temp.push(ambTemp);
-	    }
-	    
-	   }
-	   mongo.getTempMinMax(function(err,result){
-		     var maxObj=[];
-			 var minObj=[];
-			 var maxAmb=[];
-			 var minAmb=[];
-	    if(err){
-	     throw err;
-	    } 
-	    else{
-	     var recs=JSON.parse(JSON.stringify(result));
-	     console.log(result.length);
-	     
-	     var date;
-	     for(var k=result.length-1;k>=0;k--){
-	      if(result[k]._id!=""){
-	      date = Date.parse(result[k]._id);
-	      var objTemp=[];
-	      var objTemp1=[];
-	      var objTemp2=[];
-	      var objTemp3=[];
-	          objTemp[0] = date;
-	          objTemp[1] = result[k].objTempMax;
-	          objTemp1[0] = date;
-	          objTemp1[1] = result[k].objTempMin;
-	          objTemp2[0] = date;
-	          objTemp2[1] = result[k].ambTempMax;
-	          objTemp3[0] = date;
-	          objTemp3[1] = result[k].ambTempMin;
-	          maxObj.push(objTemp);
-	          minObj.push(objTemp1);
-	          maxAmb.push(objTemp2);
-	          minAmb.push(objTemp3);
-	      }
-	      
-	     }
-	     finalJson.push({"chart1":{"dates":dates,"ambTemp":temp}}, {"chart2":{"maxO": maxObj, "minO": minObj, "maxA":maxAmb, "minA": minAmb}});
-	     console.log(JSON.stringify(finalJson).replace(/\"/g, ""));
-	     res.render('charts',{"minObj":JSON.stringify(finalJson).replace(/\"/g, "")});
-	     // write another function..and so on and close accordingly
-	    }
-	    
-	   });
-	
-	  }
-	 });
-	  
+	var dates=[];
+	var temp=[];
+	var finalJson=[];
+	var piechart=[];
+	mongo.getSplineChartData(function(err,reslt){
+		if(err){
+			throw err;
+		}else{
+			var recs=JSON.parse(JSON.stringify(reslt));
+			console.log(reslt.length);
+
+			var date;
+			for(var k=0;k<reslt.length;k++){
+				if(reslt[k]._id!=""){
+					date = Date.parse(reslt[k]._id);
+					var objTemp=[];
+					var ambTemp=[];
+					objTemp[0] = date;
+					ambTemp[0]= date;
+					objTemp[1] = reslt[k].objTemp;
+					ambTemp[1] = reslt[k].ambTemp;
+					dates.push(objTemp);
+					temp.push(ambTemp);
+				}
+
+			}
+			mongo.getTempMinMax(function(err,result){
+				var maxObj=[];
+				var minObj=[];
+				var maxAmb=[];
+				var minAmb=[];
+				if(err){
+					throw err;
+				} 
+				else{
+					var recs=JSON.parse(JSON.stringify(result));
+					
+
+					var date;
+					for(var k=result.length-1;k>=0;k--){
+						if(result[k]._id!=""){
+							date = Date.parse(result[k]._id);
+							var objTemp=[];
+							var objTemp1=[];
+							var objTemp2=[];
+							var objTemp3=[];
+							objTemp[0] = date;
+							objTemp[1] = result[k].objTempMax;
+							objTemp1[0] = date;
+							objTemp1[1] = result[k].objTempMin;
+							objTemp2[0] = date;
+							objTemp2[1] = result[k].ambTempMax;
+							objTemp3[0] = date;
+							objTemp3[1] = result[k].ambTempMin;
+							maxObj.push(objTemp);
+							minObj.push(objTemp1);
+							maxAmb.push(objTemp2);
+							minAmb.push(objTemp3);
+						}
+
+					}
+					
+					
+					mongo.getPieChartTemp(function(err,piereslt){
+						if(err){
+							throw err;
+						}else{
+							var recs=JSON.stringify(piereslt);
+							console.log("retrun from the pie chart::::"+piereslt.length);
+							
+							/*piechart.push({"name":"abc", "y":reslt[0].count10},
+									{"name":"def", "y":reslt[0].count30},
+									{"name":"dgh", "y":reslt[0].count60},
+									{"name":"jkl", "y":reslt[0].count90},
+									{"name":"nmk", "y":reslt[0].count120},
+									{"name":"iop", "y":reslt[0].count150}
+									);
+							var newpie =JSON.parse(piechart);*/
+								console.log(piereslt);
+					finalJson.push({"chart1":{"dates":dates,"ambTemp":temp}}, {"chart2":{"maxO": maxObj, "minO": minObj, "maxA":maxAmb, "minA": minAmb}});
+					
+					res.render('charts',{"minObj":JSON.stringify(finalJson).replace(/\"/g, ""),"pieChart":JSON.stringify(piereslt).replace(/\"/g, "")});
+					
+					// write another function..and so on and close accordingly
+					
+						}
+					});
+				}
+
+			});
+
+		}
 	});
+
+});
 
 
 app.post("/suggestedValue",function(req,res){
 	var dp=req.body.datapoint;
 	console.log("In post  "+dp);
 	if(dp=="Temperature"){
-	mongo.suggestTempValue(function(err,result){
-		if(err){
-			throw err;
-		}else{
-			var r=result;
-			console.log("average temp  "+r);
-			mongo.insertPredictedValue(function(err,resl){
-				if(err){
-					throw err;
-				}else{
-					console.log("r"+r);
-			res.setHeader('Content-Type','application/json');
-					res.send(JSON.stringify({rsl:r}));
+		mongo.suggestTempValue(function(err,result){
+			if(err){
+				throw err;
+			}else{
+				var r=result;
+				console.log("average temp  "+r);
+				mongo.insertPredictedValue(function(err,resl){
+					if(err){
+						throw err;
+					}else{
+						console.log("r"+r);
+						res.setHeader('Content-Type','application/json');
+						res.send(JSON.stringify({rsl:r}));
 
-				}
-			},dp,r);
-		}
-	});
+					}
+				},dp,r);
+			}
+		});
 	}else if(dp=="Humidity"){
 		mongo.suggestHumdValue(function(err,result){
 			if(err){
@@ -168,97 +191,115 @@ app.post("/suggestedValue",function(req,res){
 				},dp,result);
 			}
 		});
-		}else if(dp=="Pressure"){
-			mongo.suggestPressValue(function(err,result){
-				if(err){
-					throw err;
-				}else{
-					console.log("average Press  "+result);
-					mongo.insertPredictedValue(function(err,resl){
-						if(err){
-							throw err;
-						}else{
-							console.log("resl"+resl);
-						}
-					},dp,result);
-				}
-			});
+	}else if(dp=="Pressure"){
+		mongo.suggestPressValue(function(err,result){
+			if(err){
+				throw err;
+			}else{
+				console.log("average Press  "+result);
+				mongo.insertPredictedValue(function(err,resl){
+					if(err){
+						throw err;
+					}else{
+						console.log("resl"+resl);
+					}
+				},dp,result);
 			}
+		});
+	}
 });
 
 var io = require('socket.io').listen(app.listen(3000,function(){
-    console.log("We have started our server on port 3000");
-   // SensorTag.discover(function(tag) { and close it with }); above ondiscover mthod
-    function onDiscover(tag){
-		
+	console.log("We have started our server on port 3000");
+	// SensorTag.discover(function(tag) { and close it with }); above ondiscover mthod
+	function onDiscover(tag){
+
 		tag.on('disconnect', function() {
 			console.log('disconnected!');
 			process.exit(0);
 		});
-		
+
 		function connectAndSetUpMe() {			// attempt to connect to the tag
-		     console.log('connectAndSetUp' + tag.id);
-		     tag.connectAndSetUp(enableDataPoints);	// when you connect, call enableIrTempMe
-		    
-		   }
-		     
-			function enableDataPoints(){
-				console.log('enabling Temp datapoint');
-				tag.enableIrTemperature(notifyMe);
-				tag.enableHumidity(notifyHumd);
-				tag.enableBarometricPressure(notifyPress);
-			}	
-			
-			function notifyMe(){
-				console.log("notifying temp datapoints");
-				tag.notifyIrTemperature(listenForReading);
-			}
-			function notifyHumd(){
-				console.log("notifying humd datapoints");
-				tag.notifyHumidity(listenForHumdReading);
-			}
-			function notifyPress(){
-				console.log("notify pressure");
-				tag.notifyBarometricPressure(listenForPress);
-			}
-			
-				function  listenForReading(){		
-					tag.on('irTemperatureChange', function(objectTemp, ambientTemp) {
-						
-					     console.log('\tObject Temp = %d deg. C', objectTemp.toFixed(1));
-					     function TempChange() {
-					       	io.sockets.emit('objTemp', { sensorId:tag.id, objTemp: objectTemp, ambTemp: ambientTemp});
-					       };
-					      TempChange();	 
-					       
-					   });
-				}
-				
-					function  listenForHumdReading(){
-						tag.on('humidityChange', function(temperature, humidity){
-					
-					     function HumdChange() {
-					       	io.sockets.emit('humTemp', { sensorId:tag.id,humTemp: temperature,humidity: humidity });
-					       
-					       };
-					       HumdChange();
-					      
-					   });
-				}
-					
-					function  listenForPress(){
-							tag.on('barometricPressureChange', function(pressure){
-								 console.log('\tpressure = %d', pressure.toFixed(1));
-					     function PressChange() {
-					       	io.sockets.emit('Pressure', {sensorId:tag.id, press: pressure }); 	
-					       };
-					       PressChange();
-					      
-					   });
-				}
-				connectAndSetUpMe();
+			console.log('connectAndSetUp' + tag.id);
+			tag.connectAndSetUp(enableDataPoints);	// when you connect, call enableIrTempMe
+
+		}
+
+		function enableDataPoints(){
+			console.log('enabling Temp datapoint');
+			tag.enableIrTemperature(notifyMe);
+			tag.enableHumidity(notifyHumd);
+			tag.enableBarometricPressure(notifyPress);
+			tag.enableAccelerometer(notifyAccel);
+		}	
+
+		function notifyMe(){
+			console.log("notifying temp datapoints");
+			tag.notifyIrTemperature(listenForReading);
+		}
+		function notifyHumd(){
+			console.log("notifying humd datapoints");
+			tag.notifyHumidity(listenForHumdReading);
+		}
+		function notifyPress(){
+			console.log("notify pressure");
+			tag.notifyBarometricPressure(listenForPress);
+		}
+		function notifyAccel(){
+			console.log("notify Accerlerometer");
+			tag.notifyAccelerometer(listenForAcc);
+		}
+
+		function  listenForReading(){		
+			tag.on('irTemperatureChange', function(objectTemp, ambientTemp) {
+
+				console.log('\tObject Temp = %d deg. C', objectTemp.toFixed(1));
+				function TempChange() {
+					io.sockets.emit('objTemp', { sensorId:tag.id, objTemp: objectTemp, ambTemp: ambientTemp});
+				};
+				TempChange();	 
+
+			});
+		}
+
+		function  listenForHumdReading(){
+			tag.on('humidityChange', function(temperature, humidity){
+
+				function HumdChange() {
+					io.sockets.emit('humTemp', { sensorId:tag.id,humTemp: temperature,humidity: humidity });
+
+				};
+				HumdChange();
+
+			});
+		}
+
+		function  listenForPress(){
+			tag.on('barometricPressureChange', function(pressure){
+				console.log('\tpressure = %d', pressure.toFixed(1));
+				function PressChange() {
+					io.sockets.emit('Pressure', {sensorId:tag.id, press: pressure }); 	
+				};
+				PressChange();
+
+			});
+		}
+		function  listenForAcc(){
+			tag.on('accelerometerChange', function(x,y,z){
+
+				function AccChange() {
+					io.sockets.emit('Accelero', { sensorId:tag.id,acc: x, ler: y, met:z });
+
+				};
+				AccChange();
+
+			});
+		}
+
+
+		connectAndSetUpMe();
 	}
-    SensorTag.discover(onDiscover);
+	SensorTag.discover(onDiscover);
 })
 );
 
--- cmpe295B_siemtNew/routes/mongoapi.js
+++ cmpe295B_siemtNew/routes/mongoapi.js
@@ -16,12 +16,12 @@ function createUser(callback, userfirstname, userlastname, username, email,
 			} else {
 				db.collection('user', function(err, collection) {
 					doc = {
-						"FirstName" : userfirstname,
-						"LastName" : userlastname,
-						"userName" : username,
-						"Email" : email,
-						"password" : password,
-						"userprofile" : userprofile
+							"FirstName" : userfirstname,
+							"LastName" : userlastname,
+							"userName" : username,
+							"Email" : email,
+							"password" : password,
+							"userprofile" : userprofile
 					}
 					collection.insert(doc, function(err, res) {
 						if (err) {
@@ -122,11 +122,11 @@ function getRules(callback){
 			}else{
 				var i=0;
 				var cursor=db.collection('Rule').find({}).toArray(function(err, docs) {
-				    console.log(docs.length);
-				    console.log("Found the following records");
-				    console.log(docs);
-				    callback(null,docs);
-				  });	
+					console.log(docs.length);
+					console.log("Found the following records");
+					console.log(docs);
+					callback(null,docs);
+				});	
 			}
 		});
 	});
@@ -143,9 +143,9 @@ function getSplineChartData(callback){
 				throw err;
 			}else{
 				var cursor=db.collection('temp').aggregate([{$group : {_id : { $substr: [ "$timeStamp", 0, 10] }, objTemp : {$avg : "$ObjectTemp"},ambTemp:{$avg : "$AmbientTemp"}}},{ $sort : { _id : 1 } }]).toArray(function(err, docs) {
-				    
-				    callback(null,docs);
-				  });	
+
+					callback(null,docs);
+				});	
 			}
 		});
 	});
@@ -153,26 +153,68 @@ function getSplineChartData(callback){
 
 function getTempMinMax(callback){
 	db=new mongodb.Db('cmpe295b_siemt', new mongodb.Server('ds045704.mongolab.com', 45704, {auto_reconnect:true}), {});
-    
+
 	db.open(function(err, db) {
 		db.authenticate('username','password',function(err){
 			if(err){
 				throw err;
 			}else{
 				var cursor=	db.collection('temp').aggregate( [{ $match: { $and: [{ ObjectTemp: {$lt: 90, $gt: 0 } }, { AmbientTemp: {$lt: 90, $gt: 0 } }] }},{ $group: {_id: {$substr: ["$timeStamp", 0, 10]}, objTempMax: { $max: "$ObjectTemp" }, objTempMin: { $min: "$ObjectTemp" }, ambTempMax: { $max: "$AmbientTemp" }, ambTempMin: { $min: "$AmbientTemp" } } },
-				           	                 {$sort: {_id: -1}}, {$limit:7}] ).toArray(function(err, docs){
-					console.log(docs.length);
-				    console.log("Found the following in max min records");
-				    console.log(docs);
-				    callback(null,docs);
+				           	                                  {$sort: {_id: -1}}, {$limit:7}] ).toArray(function(err, docs){
+				           	                                	  console.log(docs.length);
+				           	                                	  console.log("Found the following in max min records");
+				           	                                	  console.log(docs);
+				           	                                	  callback(null,docs);
 
-				  });
-				
-			
+				           	                                  });
 			}
 		});
 	});
 }
+
+
+function getPieChartTemp(callback){
+	console.log("getting pie chart temp and timestamp");
+	db=new mongodb.Db('cmpe295b_siemt', new mongodb.Server('ds045704.mongolab.com', 45704, {auto_reconnect:true}), {});
+	db.open(function(err, db) {
+		db.authenticate('username','password',function(err){
+			if(err){
+				throw err;
+			}else{
+				var cursor=db.collection('temp').aggregate([
+				                                            { 
+				                                            	 $project: 
+				                                            	 { 
+				                                            	 day :{ $substr: [ "$timeStamp", 8, 2]}, 
+				                                            	 lessThan10: { $cond: [ { $and: [ { $gte: [ "$ObjectTemp", 0 ] }, { $lt: [ "$ObjectTemp", 18 ] } ] }, 1, 0]}, 
+				                                            	 lessThan30: { $cond: [ { $and: [ { $gte: [ "$ObjectTemp", 18 ] }, { $lt: [ "$ObjectTemp", 30 ] } ] }, 1, 0]},
+				                                            	 lessThan60: { $cond: [ { $and: [ { $gte: [ "$ObjectTemp", 30 ] }, { $lt: [ "$ObjectTemp", 60 ] } ] }, 1, 0]}, 
+				                                            	 lessThan90: { $cond: [ { $and: [ { $gte: [ "$ObjectTemp", 60 ] }, { $lt: [ "$ObjectTemp", 90 ] } ] }, 1, 0]},
+				                                            	 lessThan120: {$cond: [ { $and: [ { $gte: [ "$ObjectTemp", 90 ] }, { $lt: [ "$ObjectTemp", 120 ] } ] }, 1, 0]},
+				                                            	 moreThan10: {$cond: [ { $and: [ { $gte: [ "$ObjectTemp", 120 ] }, { $lt: [ "$ObjectTemp", 250 ] } ] }, 1, 0]}
+				                                            	 } 
+				                                            	 },  
+				                                            	 { 
+
+				                                            	 $group: { 
+				                                            	  _id : "$day", 
+				                                            	  count10: { $sum: "$lessThan10" },
+				                                            	  count30: { $sum: "$lessThan30" }, 
+				                                            	  count60: { $sum: "$lessThan60" },
+				                                            	  count90: { $sum: "$lessThan90" },
+				                                            	  count120: { $sum: "$lessThan120" },
+				                                            	  count150: { $sum: "$moreThan10" } 
+				                                            	  } 
+				                                            	  }, {$sort: { _id : -1 }}, {$limit:1}
+				                                            	 ]).toArray(function(err, docs) {
+
+					callback(null,docs);
+				});	
+			}
+		});
+	});
+}
+
 
 
 function suggestTempValue(callback){
@@ -185,18 +227,18 @@ function suggestTempValue(callback){
 			}else{
 				var cursor=db.collection('temp').aggregate([{$group : {_id : { $substr: [ "$timeStamp", 0, 10] }, objTemp : {$avg : "$ObjectTemp"}}},{ $sort : { _id : -1 } },{$limit:5}]).toArray(function(err,docs){
 					console.log(docs.length);
-				    console.log("Found the following records");
-				    console.log(docs);
-				    var sumTemp=0;
-				    var objTempRecommndedvalu=0;
-				    for(var j=0;j<docs.length;j++){
-				    	console.log(docs[j].objTemp);
-				    	sumTemp=sumTemp+(docs[j].objTemp);
-				    	console.log(sumTemp);
-				    }
-				    objTempRecommndedvalu=sumTemp/(docs.length);
-				    console.log("avg"+objTempRecommndedvalu);
-				    callback(null,objTempRecommndedvalu);
+					console.log("Found the following records");
+					console.log(docs);
+					var sumTemp=0;
+					var objTempRecommndedvalu=0;
+					for(var j=0;j<docs.length;j++){
+						console.log(docs[j].objTemp);
+						sumTemp=sumTemp+(docs[j].objTemp);
+						console.log(sumTemp);
+					}
+					objTempRecommndedvalu=sumTemp/(docs.length);
+					console.log("avg"+objTempRecommndedvalu);
+					callback(null,objTempRecommndedvalu);
 				});
 			}
 		});
@@ -213,18 +255,18 @@ function suggestHumdValue(callback){
 			}else{
 				var cursor=db.collection('humidity').aggregate([{$group : {_id : { $substr: [ "$timeStamp", 0, 10] }, humid : {$avg : "$Humidity"}}},{ $sort : { _id : -1 } },{$limit:5}]).toArray(function(err,docs){
 					console.log(docs.length);
-				    console.log("Found the following records");
-				    console.log(docs);
-				    var sumhumd=0;
-				    var humidRecommndedvalu=0;
-				    for(var j=0;j<docs.length;j++){
-				    	console.log(docs[j].humid);
-				    	sumhumd=sumhumd+(docs[j].humid);
-				    	console.log(sumhumd);
-				    }
-				    humidRecommndedvalu=sumhumd/(docs.length);
-				    console.log("avg "+humidRecommndedvalu);
-				    callback(null,humidRecommndedvalu);
+					console.log("Found the following records");
+					console.log(docs);
+					var sumhumd=0;
+					var humidRecommndedvalu=0;
+					for(var j=0;j<docs.length;j++){
+						console.log(docs[j].humid);
+						sumhumd=sumhumd+(docs[j].humid);
+						console.log(sumhumd);
+					}
+					humidRecommndedvalu=sumhumd/(docs.length);
+					console.log("avg "+humidRecommndedvalu);
+					callback(null,humidRecommndedvalu);
 				});
 			}
 		});
@@ -241,18 +283,18 @@ function suggestPressValue(callback){
 			}else{
 				var cursor=db.collection('pressure').aggregate([{$group : {_id : { $substr: [ "$timeStamp", 0, 10] }, press : {$avg : "$Pressure"}}},{ $sort : { _id : -1 } },{$limit:5}]).toArray(function(err,docs){
 					console.log(docs.length);
-				    console.log("Found the following records");
-				    console.log(docs);
-				    var sumpress=0;
-				    var pressRecommndedvalu=0;
-				    for(var j=0;j<docs.length;j++){
-				    	console.log(docs[j].press);
-				    	sumpress=sumpress+(docs[j].press);
-				    	console.log(sumpress);
-				    }
-				    pressRecommndedvalu=sumpress/(docs.length);
-				    console.log("avg "+pressRecommndedvalu);
-				    callback(null,pressRecommndedvalu);
+					console.log("Found the following records");
+					console.log(docs);
+					var sumpress=0;
+					var pressRecommndedvalu=0;
+					for(var j=0;j<docs.length;j++){
+						console.log(docs[j].press);
+						sumpress=sumpress+(docs[j].press);
+						console.log(sumpress);
+					}
+					pressRecommndedvalu=sumpress/(docs.length);
+					console.log("avg "+pressRecommndedvalu);
+					callback(null,pressRecommndedvalu);
 				});
 			}
 		});
@@ -274,23 +316,23 @@ function insertPredictedValue(callback,datapoint,predVal){
 			}else{
 				console.log("db.collection('predictionValues').update( { \"_id\" : \"pid\" },{ $set: {"+datapoint1+":"+predVal + "} }");
 				if(datapoint=="Temperature"){
-				db.collection('predictionValues').update(
-						{
-							"_id" : "pid"
-						},
-						{
-							$set : {
-								"Temperature": predVal
-							}
-						},
-						function(err, results) {
-							if (err) {
-								throw err;
-							} else {
-								console.log("updated suggested value"+ JSON.stringify(results));
-								callback(null, results);
-							}
-						});
+					db.collection('predictionValues').update(
+							{
+								"_id" : "pid"
+							},
+							{
+								$set : {
+									"Temperature": predVal
+								}
+							},
+							function(err, results) {
+								if (err) {
+									throw err;
+								} else {
+									console.log("updated suggested value"+ JSON.stringify(results));
+									callback(null, results);
+								}
+							});
 				}else if(datapoint=="Humidity"){
 					db.collection('predictionValues').update(
 							{
@@ -309,33 +351,33 @@ function insertPredictedValue(callback,datapoint,predVal){
 									callback(null, results);
 								}
 							});
-					}else if(datapoint=="Pressure"){
-						db.collection('predictionValues').update(
-								{
-									"_id" : "pid"
-								},
-								{
-									$set : {
-										"pressure": predVal
-									}
-								},
-								function(err, results) {
-									if (err) {
-										throw err;
-									} else {
-										console.log("updated suggested value"+ JSON.stringify(results));
-										callback(null, results);
-									}
-								});
-						}
+				}else if(datapoint=="Pressure"){
+					db.collection('predictionValues').update(
+							{
+								"_id" : "pid"
+							},
+							{
+								$set : {
+									"pressure": predVal
+								}
+							},
+							function(err, results) {
+								if (err) {
+									throw err;
+								} else {
+									console.log("updated suggested value"+ JSON.stringify(results));
+									callback(null, results);
+								}
+							});
+				}
 //				console.log("{\"_id\":\""+pid+"\"},[['_id','asc']],{$set:{"+datapoint1+":"+predVal+"}},{}");
 //				db.collection("predictionValues").findAndModify({"_id":"pid"},[['_id','asc']],{$set:{datapoint1:predVal}},{},function(err,reslt){
-//					if(err){
-//						console.log("i'm error");
-//						throw err;
-//					}else{
-//						callback(null,reslt);
-//					}
+//				if(err){
+//				console.log("i'm error");
+//				throw err;
+//				}else{
+//				callback(null,reslt);
+//				}
 //				});
 			}
 		});
@@ -685,13 +727,14 @@ function fetchHumidityCharts(callback, chartname) {
 		});
 
 	}
-*/
+ */
 
 exports.createUser=createUser;
 exports.checkUser=checkUser;
 exports.updatePassword=updatePassword;
 exports.getRules=getRules;
 exports.getSplineChartData=getSplineChartData;
+exports.getPieChartTemp=getPieChartTemp;
 exports.getTempMinMax=getTempMinMax;
 exports.suggestTempValue=suggestTempValue;
 exports.suggestHumdValue=suggestHumdValue;
@@ -700,4 +743,3 @@ exports.insertPredictedValue=insertPredictedValue;
 //exports.insertTemp=insertTemp;
 //exports.insertHumd=insertHumd;
 //exports.insertPress=insertPress;
-	
