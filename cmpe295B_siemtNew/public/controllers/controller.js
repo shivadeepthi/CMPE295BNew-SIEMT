5d2d492ff4f25ab4ee0a217408c2472b8cbb0b03
anshul2117 <anshul2102@gmail.com>
2015-12-07 12:41:18 -0800

    added no records found ng-if

--- cmpe295B_siemtNew/public/controllers/controller.js
+++ cmpe295B_siemtNew/public/controllers/controller.js
@@ -2,16 +2,16 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
 
-    var refresh = function() {
-  $http.get('/getValue').success(function(response) {
-    console.log("I got the data I requested");
-    $scope.datalist = response;
-    $scope.datapoint = "";
-  });
+//     var refresh = function() {
+//   $http.get('/getValue').success(function(response) {
+//     console.log("I got the data I requested");
+//     $scope.datalist = response;
+//     $scope.datapoint = "";
+//   });
 
-};
+// };
 
-refresh();
+// refresh();
 
 
 $scope.checkOptions = function() {
@@ -21,7 +21,8 @@ $scope.checkOptions = function() {
     console.log(response);
 
     $scope.datalist = response;
-    $scope.datapoint = "";
+
+    $scope.datapoint = "No data";
 
 
   });
